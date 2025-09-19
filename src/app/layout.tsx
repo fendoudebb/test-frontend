6a4c1835483833a39737c81a1382c630ad153507
fendoudebb <15317832520@163.com>
2025-09-19 16:26:04 +0800

    [feature]: update context

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,6 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import {RegisterProvider} from "@/app/(_portal)/_context/RegisterContext";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -25,9 +15,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`flex flex-col justify-center`}
       >
-        {children}
+          <RegisterProvider>
+              <div className="flex-1 flex items-center justify-center">
+                  {children}
+              </div>
+          </RegisterProvider>
       </body>
     </html>
   );
