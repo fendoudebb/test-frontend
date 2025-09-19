b7838172b49037fe94c6f7b757c46344b8134a2d
fendoudebb <15317832520@163.com>
2025-09-19 16:48:40 +0800

    [feature]: step2&3

--- src/app/(_portal)/step2/page.tsx
+++ src/app/(_portal)/step2/page.tsx
@@ -1,3 +1,46 @@
-export default function Step2() {
+'use client'
 
+import {useRegisterContext} from "@/app/(_portal)/_context/RegisterContext";
+import {useRouter} from "next/navigation";
+
+export default function Step2() {
+    const {formData, updateFormData} = useRegisterContext();
+    const router = useRouter();
+    const {country, gender, avatar} = formData.step2;
+    const doSubmit = (e: React.FormEvent) => {
+        e.preventDefault();
+        if (!country || !gender) {
+            return alert("Please fill out the form");
+        }
+        router.push("/step3")
+    }
+    return (
+        <form onSubmit={doSubmit} style={{minWidth: 500}}>
+            <h2>Step2</h2>
+            <div className="flex gap-1">
+                <label className="basis-1/4">Country</label>
+                <input className="basis-3/4 border border-gray-300" type={"text"} required value={country} onChange={(e) => {
+                    updateFormData('step2', {country: e.target.value});
+                }}/>
+            </div>
+            <div className="flex gap-1">
+                <label className="basis-1/4">Gender</label>
+                <select className="basis-3/4 border border-gray-300" required value={gender} onChange={(e) => {
+                    updateFormData('step2', {gender: e.target.value});
+                }}>
+                    <option value="">Choose the gender</option>
+                    <option value="male">Male</option>
+                    <option value="female">Female</option>
+                    <option value="other">Other</option>
+                </select>
+            </div>
+            <div className="flex gap-1">
+                <label className="basis-1/4">Avatar</label>
+                <input className="basis-3/4 border border-gray-300" type={"file"} value={avatar} onChange={(e) => {
+                    updateFormData('step2', {avatar: e.target.value});
+                }}/>
+            </div>
+            <button type={"submit"}>Next</button>
+        </form>
+    )
 };
--- src/app/(_portal)/step3/page.tsx
+++ src/app/(_portal)/step3/page.tsx
@@ -1,3 +1,35 @@
-export default function Step3() {
+'use client'
 
+import {useRegisterContext} from "@/app/(_portal)/_context/RegisterContext";
+import {useRouter} from "next/navigation";
+
+export default function Step3() {
+    const {formData, updateFormData} = useRegisterContext();
+    const router = useRouter();
+    const {email, password} = formData.step3;
+    const doSubmit = (e: React.FormEvent) => {
+        e.preventDefault();
+        if (!email || !password) {
+            return alert("Please fill out the form");
+        }
+        router.push("/step4")
+    }
+    return (
+        <form onSubmit={doSubmit} className="shadow" style={{minWidth: 500}}>
+            <h2>Step3</h2>
+            <div className="flex gap-1">
+                <label className="basis-1/4">Email</label>
+                <input className="basis-3/4 border border-gray-300" type={"email"} required value={email} onChange={(e) => {
+                    updateFormData('step3', {email: e.target.value});
+                }}/>
+            </div>
+            <div className="flex gap-1">
+                <label className="basis-1/4">Password</label>
+                <input className="basis-3/4 border border-gray-300" type={"password"} required minLength={8} value={password} onChange={(e) => {
+                    updateFormData('step3', {password: e.target.value});
+                }}/>
+            </div>
+            <button type={"submit"} className="cursor-pointer">Submit</button>
+        </form>
+    )
 };
