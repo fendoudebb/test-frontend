f539263b59e5126604e74033ea5281b073f57d38
fendoudebb <15317832520@163.com>
2025-09-19 16:56:09 +0800

    [feature]: step4

--- src/app/(_portal)/step4/page.tsx
+++ src/app/(_portal)/step4/page.tsx
@@ -1,3 +1,61 @@
-export default function Step4() {
+'use client'
 
+import {useRegisterContext} from "@/app/(_portal)/_context/RegisterContext";
+
+export default function Step4() {
+    const {formData} = useRegisterContext();
+    return (
+        <div className="flex flex-col gap-2 p-2" style={{backgroundColor: 'white', minWidth: 500}}>
+            <div className="flex gap-1">
+                <label className="basis-1/4">FirstName: </label>
+                <span className="basis-3/4">
+                    {formData.step1.firstName}
+                </span>
+            </div>
+            <div className="flex gap-1">
+                <label className="basis-1/4">LastName: </label>
+                <span className="basis-3/4">
+                    {formData.step1.lastName}
+                </span>
+            </div>
+            <div className="flex gap-1">
+                <label className="basis-1/4">BirthDate: </label>
+                <span className="basis-3/4">
+                    {formData.step1.birthDate}
+                </span>
+            </div>
+
+            <div className="flex gap-1">
+                <label className="basis-1/4">Country: </label>
+                <span className="basis-3/4">
+                    {formData.step2.country}
+                </span>
+            </div>
+            <div className="flex gap-1">
+                <label className="basis-1/4">Gender: </label>
+                <span className="basis-3/4">
+                    {formData.step2.gender}
+                </span>
+            </div>
+            <div className="flex gap-1">
+                <label className="basis-1/4">Avatar: </label>
+                <span className="basis-3/4">
+                    {formData.step2.avatar}
+                </span>
+            </div>
+
+            <div className="flex gap-1">
+                <label className="basis-1/4">Email: </label>
+                <span className="basis-3/4">
+                    {formData.step3.email}
+                </span>
+            </div>
+            <div className="flex gap-1">
+                <label className="basis-1/4">Password: </label>
+                <span className="basis-3/4">
+                    {formData.step3.password}
+                </span>
+            </div>
+        </div>
+    )
 };
